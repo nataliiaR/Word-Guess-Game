a2073778938dafe7ef4c1640503f9a96cb2c2440
Nataliia Ryzha <ryzha89@gmail.com>
2019-06-06 21:30:13 -0700

    creating styles and content

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-
+    document.getElementById("show_image").style.display="none";
 function startTheGame() {
     //clear guessed word between games
+    document.getElementById("show_image").style.display="none";
     document.getElementById("word_holder").innerHTML = "";
     document.getElementById("guessed_letters").innerHTML ="";
     //show info tag after starting a game
@@ -9,8 +10,8 @@ function startTheGame() {
     //show welcome text after starting a game
     document.createElement("SPAN").appendChild(document.createTextNode("Press a key on keyboard and reveal your letter guess!"));
 
-    var bandToGuess; 
-    var bandIndex;
+    var countryToGuess; 
+    var countryIndex;
     var name;
     var userGuess;
     var characters;
@@ -18,32 +19,64 @@ function startTheGame() {
     var match=0;
     var left_letter;
 
-    function pickABandToGuess(){
-        var bandCollection =["madonna"]; //["Metalica", "Deep Purple", "Led Zeppelin"]; 
-        bandIndex = Math.floor(Math.random() * bandCollection.length);
-
-        bandToGuess = bandCollection[bandIndex];
-        return bandToGuess;
+    var country1 ={
+        name:"UKRAINE",
+        image:"assets/images/ukraine.jpg"
+    
     }
 
-    var name = pickABandToGuess();
+    var country2 ={
+        name:"ICELAND",
+        image:"assets/images/iceland.jpg"
+    }
+
+    var country3 ={
+        name:"ITALY",
+        image:"assets/images/italy.jpg"
+    }
+
+    var country4 ={
+        name:"ARGENTINA",
+        image:"assets/images/argentina.jpg"
+    }
+
+    var country5 ={
+        name:"AUSTRALIA",
+        image:"assets/images/australia.jpg"
+    }
+
+    var country6 ={
+        name:"NICARAGUA",
+        image:"assets/images/nicaragua.jpg"
+    }
 
 
-    function bandNameToArray(name){
+    
+    function pickACountryToGuess(){
+        var countryCollection =[country1, country2,country3,country4,country5,country6];
+        countryIndex = Math.floor(Math.random() * countryCollection.length);
+
+        countryToGuess = countryCollection[countryIndex];
+        return countryToGuess.name;
+    }
+
+    var name = pickACountryToGuess();
+
+
+    function countryNameToArray(name){
 
         var arrayofChars = name.split("");
         return arrayofChars;
     }
 
     function createBlurredWord(){
-        var arrayNew= bandNameToArray(name);
+        var arrayNew= countryNameToArray(name);
 
         for (i=0; i<arrayNew.length; i++){
   
             var el = document.createElement("SPAN");
             t = document.createTextNode(arrayNew[i]);
             el.appendChild(t);
-            document.getElementById('word_holder').appendChild(el).style.borderBottom="1px solid black";
             document.getElementById('word_holder').appendChild(el).style.margin="1em";
             document.getElementById('word_holder').appendChild(el).style.filter="blur(4px)";
             
@@ -93,14 +126,17 @@ function startTheGame() {
                 }
 
             }
-            if(match==amountOfLetters){
+            if(match===amountOfLetters){
                 document.getElementById("guessed_letters").innerHTML="YOU'VE GUESSED THE WORD!";
+                document.getElementById("show_image").style.display="block";
+                document.getElementById("show_image").src = countryToGuess.image;
                 document.onkeyup = null;
+                document.getElementById("info").style.display="none";
             
             }
         }
         else{
-            document.getElementById("guessed_letters").innerHTML="OH NO, YOU DID NOT GUESS THE WORD!";
+            document.getElementById("guessed_letters").innerHTML="OH NO, YOU DID NOT GUESS THE COUNTRY!";
             document.getElementById("info").style.display="none";
             document.onkeyup = null;
         }   
