3ec23297e4bf9ff881d91950a505c283140ab64e
Nataliia Ryzha <ryzha89@gmail.com>
2019-06-08 20:01:48 -0700

    added additional countries

--- script.js
+++ script.js
@@ -44,36 +44,41 @@ function startTheGame() {
     var country10 ={name:"INDIA",image:"https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/125px-Flag_of_India.svg.png",learnMore:"https://en.wikipedia.org/wiki/India"}
     var country11 ={name:"JAPAN",image:"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/125px-Flag_of_Japan.svg.png",learnMore:"https://en.wikipedia.org/wiki/Japan"}
     var country12 ={name:"GEORGIA",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flag_of_Georgia.svg/125px-Flag_of_Georgia.svg.png",learnMore:"https://en.wikipedia.org/wiki/Georgia_(country)"}
-
+    var country13 ={name:"ECUADOR",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/250px-Flag_of_Ecuador.svg.png",learnMore:"https://en.wikipedia.org/wiki/Ecuador"}
+    var country14 ={name:"NORWAY",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Norway.svg/125px-Flag_of_Norway.svg.png",learnMore:"https://en.wikipedia.org/wiki/Norway"}
+    var country15 ={name:"FINLAND",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/125px-Flag_of_Finland.svg.png",learnMore:"https://en.wikipedia.org/wiki/Finland"}
+    var country16 ={name:"AUSTRIA",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/125px-Flag_of_Austria.svg.png",learnMore:"https://en.wikipedia.org/wiki/Austria"}
+    var country17 ={name:"MONTENEGRO",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Flag_of_Montenegro.svg/125px-Flag_of_Montenegro.svg.png",learnMore:"https://en.wikipedia.org/wiki/Montenegro"}
+    var country18 ={name:"TANZANIA",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Flag_of_Tanzania.svg/125px-Flag_of_Tanzania.svg.png",learnMore:"https://en.wikipedia.org/wiki/Tanzania"}
+    var country19 ={name:"UGANDA",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Flag_of_Uganda.svg/125px-Flag_of_Uganda.svg.png",learnMore:"https://en.wikipedia.org/wiki/Uganda"}
+    var country20 ={name:"BELGIUM",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Belgium.svg/125px-Flag_of_Belgium.svg.png",learnMore:"https://en.wikipedia.org/wiki/Belgium"}
+    var country21 ={name:"MOROCCO",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/125px-Flag_of_Morocco.svg.png",learnMore:"https://en.wikipedia.org/wiki/Morocco"}
     
     function pickACountryToGuess(){
-        var countryCollection =[country1, country2,country3,country4,country5,country6,country7, country8,country9,country10,country11,country12];
+        var countryCollection =[country1, country2,country3,country4,country5,country6,country7, 
+            country8,country9,country10,country11,country12,country13,country14,country15,
+            country16, country17,country18, country19, country20, country21];
         countryIndex = Math.floor(Math.random() * countryCollection.length);
-
         countryToGuess = countryCollection[countryIndex];
         return countryToGuess.name;
     }
 
     var name = pickACountryToGuess();
 
-
     function countryNameToArray(name){
-
         var arrayofChars = name.split("");
         return arrayofChars;
     }
 
     function createBlurredWord(){
         var arrayNew= countryNameToArray(name);
-
         for (i=0; i<arrayNew.length; i++){
   
             var el = document.createElement("SPAN");
             t = document.createTextNode(arrayNew[i]);
             el.appendChild(t);
             document.getElementById('word_holder').appendChild(el).style.margin="1em";
-            document.getElementById('word_holder').appendChild(el).style.filter="blur(4px)";
-            
+            document.getElementById('word_holder').appendChild(el).style.filter="blur(4px)"; 
         }
     }
 
@@ -87,42 +92,28 @@ function startTheGame() {
         if (left_letter>0 ){
         // Determines which key was pressed.
             userGuess = event.key;
-            
-            //array to keep already pressed keys
+         //array to keep already pressed keys
             var  array= new Array();
-            //helpers
+        //helpers
             var  spanElement, textNodeinSpan;
             //create new span element to keep entered value there
             spanElement = document.createElement("SPAN");
-            
             array.push(userGuess);
             textNodeinSpan = document.createTextNode(userGuess);
             spanElement.appendChild(textNodeinSpan);
             document.getElementById("guessed_letters").appendChild(spanElement).style.margin="0.7em";
-            
             left_letter=left_letter-1;
-
-            
             document.getElementById("letters_left").innerHTML=left_letter + " ";
-
             amountOfLetters = document.getElementById('word_holder').childElementCount;
-
-
             for (i=0; i<amountOfLetters; i++){
-            
                 characters = document.getElementById('word_holder').childNodes;
-   
                 character = characters[i].innerHTML;
-
-                if (userGuess.toLowerCase() === character.toLowerCase() ){
-                    
+                if (userGuess.toLowerCase() === character.toLowerCase() ){ 
                     if(document.getElementById('word_holder').childNodes[i].style.filter!="blur(0px)"){
                         document.getElementById('word_holder').childNodes[i].style.filter="blur(0px)";
                         match= match+1;
                     }
-
                 }
-
             }
             if(match===amountOfLetters){
                 document.getElementById("guessed_letters").innerHTML="YOU'VE GUESSED THE WORD!";
@@ -133,21 +124,14 @@ function startTheGame() {
                 learnMoreLink.setAttribute('href',countryToGuess.learnMore);
                 learnMoreLink.setAttribute("id","linklearnMore");
                 document.getElementById("learnMore").appendChild(learnMoreLink);
-            
                 document.onkeyup = null;
                 document.getElementById("info").style.display="none";
-            
             }
-                    
- 
         }
-
         else{
             document.getElementById("guessed_letters").innerHTML="OH NO, YOU DID NOT GUESS THE COUNTRY!";
             document.getElementById("info").style.display="none";
             document.onkeyup = null;
         }   
     }
-
-
 }
